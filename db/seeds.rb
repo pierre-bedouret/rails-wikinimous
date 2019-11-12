65e395fdb150f43bbe0b7e9cc7926201fd826a3b
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-11-12 16:59:52 +0100

    App complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.create!(
+    title: Faker::Book.title,
+    content: Faker::ChuckNorris.fact
+  )
+end
+puts 'Finished!'
